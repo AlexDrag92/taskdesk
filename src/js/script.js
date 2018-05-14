6dede7f3afcb58028488c84463a600ffe687649a
Alexandr <alex.drag92@yandex.ru>
2018-05-14 16:41:02 +0400

    Fixed menu, slider, overlay. Added new page

--- src/js/script.js
+++ src/js/script.js
@@ -62,11 +62,11 @@ scrollElement.click(function(e) {
 });
 
 
-//
-
+//Workflow Slider
+/*
 $('.workflow__phase-title').on('click', function (event) {
   event.preventDefault();
-})
+});
 
 $('.workflow__phase').on('click', function (event) {
   var curentTitle = $(this).find('.workflow__phase-title');
@@ -78,25 +78,94 @@ $('.workflow__phase').on('click', function (event) {
    $('.workflow__image').attr('src', curentTitle.attr('href'));
  }
 
+})*/
+
+
+var phases = $('.workflow__phase');
+var titles = $('.workflow__phase-title');
+var slideImg = $('.workflow__image');
+var indxSlide = 0;
+ 
+/*
+var startSlider = setInterval(function () {
+  changeSlides();
+}, 2000);
+*/
+ 
+var changeSlides = function () {
+ 
+  if(indxSlide > phases.length-2){
+    indxSlide = 0;
+  }else{
+    indxSlide++;
+  };
+ 
+  slideImg.attr('src', $('.workflow__phase:eq('+indxSlide+')').find('.workflow__phase-title').attr('href'));
+  titles.removeClass('workflow__phase-title--red-line');
+  $('.workflow__phase:eq('+indxSlide+')').find('.workflow__phase-title').addClass('workflow__phase-title--red-line');
+};
+ 
+var playSlides = setInterval(changeSlides, 2000)
+ 
+titles.on('click', function (event) {
+  event.preventDefault();
+});
+ 
+phases.on('click', function (event) {
+ clearInterval(playSlides);
+  playSlides = setInterval(changeSlides, 2000)
+ 
+ var curentTitle = $(this).find(titles);
+ indxSlide = phases.index(this);
+ 
+ if(!curentTitle.hasClass('workflow__phase-title--red-line')){
+   titles.removeClass('workflow__phase-title--red-line');
+   curentTitle.addClass('workflow__phase-title--red-line');
+   slideImg.attr('src', curentTitle.attr('href'));
+ };
+});
+
+
+//Dropdown style
+$('.dropdown').on('show.bs.dropdown', function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+});
+
+$('.dropdown').on('hide.bs.dropdown', function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).hide();
+});
+
+
+//PUP UP s
+$('#enter-registration-button').on('click', function(event){
+  event.preventDefault()
+  $('#registration').slideDown('easeInOutQuint');
+  overlay.show()
 })
-  //console.log(curentTitle.hasClass('workflow__phase-title--red-line'));
+
+$('.modal-popup__close-button').on('click', function(){
+  $(this).parent().fadeOut();
+  overlay.hide()
+});
 
 
+$('#modal-popup__account-link').on('click', function(event){
+  event.preventDefault();
+  $(this).parent().slideUp();
+  $('#account-popup').slideDown();
+});
 
-  /*if($(this).hasClass('workflow__phase-title--red-line') !== true){
-    $('.workflow__phase-title').removeClass('workflow__phase-title--red-line');
-    $(this).addClass('workflow__phase-title--red-line');
-    $('.workflow__image').attr('src', $(this).attr('href'));
 
-    console.log($('.workflow__image').attr('href'))
-  }*/
+var overlay = $('.overlay');
+overlay.css('height' , $(document).height()+'px');
 
-//});
+overlay.on('click', function(){
+  $(this).hide();
+  $('.modal-popup').each(function(){
+    $(this).hide()
+  })
 
-    $('.dropdown').on('show.bs.dropdown', function() {
-      $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
-    });
+});
 
-    $('.dropdown').on('hide.bs.dropdown', function() {
-      $(this).find('.dropdown-menu').first().stop(true, true).hide();
-    });
+
+//console.log($(document).height()+'px')
