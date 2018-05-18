5d382b6911c3d9e6dc19b62253293ef6ce24938b
Alex Drag <alex.drag@yandex.ru>
2018-05-18 16:59:38 +0300

    Added some styles for bid form

--- src/js/script.js
+++ src/js/script.js
@@ -19,35 +19,35 @@ var phases = $('.workflow__phase');
 var titles = $('.workflow__phase-title');
 var slideImg = $('.workflow__image');
 var indxSlide = 0;
- 
+
 /*
 var startSlider = setInterval(function () {
   changeSlides();
 }, 2000);
 */
- 
+
 var changeSlides = function () {
- 
+
   if(indxSlide > phases.length-2){
     indxSlide = 0;
   }else{
     indxSlide++;
   };
- 
+
   slideImg.attr('src', $('.workflow__phase:eq('+indxSlide+')').find('.workflow__phase-title').attr('href'));
   titles.removeClass('workflow__phase-title--red-line');
   $('.workflow__phase:eq('+indxSlide+')').find('.workflow__phase-title').addClass('workflow__phase-title--red-line');
 };
- 
+
 var playSlides = setInterval(changeSlides, 2000)
- 
+
 titles.on('click', function (event) {
   event.preventDefault();
 });
- 
+
 phases.on('click', function (event) {
  clearInterval(playSlides);
-  playSlides = setInterval(changeSlides, 2000)
+ playSlides = setInterval(changeSlides, 2000)
  
  var curentTitle = $(this).find(titles);
  indxSlide = phases.index(this);
@@ -116,7 +116,99 @@ menuButton.on('click', function(){
 });
 
 
+//DatePicker langs
+var lang = "ru";
 
-$(document).ready(function(){
-  $('#time').datepicker()
+( function( factory ) {
+  if ( typeof define === "function" && define.amd ) {
+    define( [ "../widgets/datepicker" ], factory );
+  } else {
+    factory( jQuery.datepicker );
+  }
+}( function( datepicker ) {
+
+  datepicker.regional.ro = {
+    closeText: "Închide",
+    prevText: "&#xAB; Luna precedentă",
+    nextText: "Luna următoare &#xBB;",
+    currentText: "Azi",
+    monthNames: [ "Ianuarie","Februarie","Martie","Aprilie","Mai","Iunie",
+    "Iulie","August","Septembrie","Octombrie","Noiembrie","Decembrie" ],
+    monthNamesShort: [ "Ian", "Feb", "Mar", "Apr", "Mai", "Iun",
+    "Iul", "Aug", "Sep", "Oct", "Nov", "Dec" ],
+    dayNames: [ "Duminică", "Luni", "Marţi", "Miercuri", "Joi", "Vineri", "Sâmbătă" ],
+    dayNamesShort: [ "Dum", "Lun", "Mar", "Mie", "Joi", "Vin", "Sâm" ],
+    dayNamesMin: [ "Du","Lu","Ma","Mi","Jo","Vi","Sâ" ],
+    weekHeader: "Săpt",
+    dateFormat: "dd.mm.yy",
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: ""
+  };
+
+  datepicker.regional.ru = {
+    closeText: "Закрыть",
+    prevText: "&#x3C;Пред",
+    nextText: "След&#x3E;",
+    currentText: "Сегодня",
+    monthNames: [ "Январь","Февраль","Март","Апрель","Май","Июнь",
+    "Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
+    monthNamesShort: [ "Янв","Фев","Мар","Апр","Май","Июн",
+    "Июл","Авг","Сен","Окт","Ноя","Дек" ],
+    dayNames: [ "воскресенье","понедельник","вторник","среда","четверг","пятница","суббота" ],
+    dayNamesShort: [ "вск","пнд","втр","срд","чтв","птн","сбт" ],
+    dayNamesMin: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
+    weekHeader: "Нед",
+    dateFormat: "dd.mm.yy",
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: ""
+  };
+
+  if( lang === "ru"){
+    datepicker.setDefaults( datepicker.regional.ru);
+  }else if( lang === "ro"){
+    datepicker.setDefaults( datepicker.regional.ro);
+  }
+
+} ) );
+
+
+//datePickers
+$('.bud__start-area-date').datepicker({
+  constrainInput: false,
+  dateFormat: 'dd.mm.y'
+})
+
+$('.bud__finish-area-date').datepicker({
+  constrainInput: false,
+  dateFormat: 'dd.mm.y'
+})
+
+//Toggle inputs
+$('.bid__period-select').on('click', function(){
+  if($(this).val() == 'period'){
+    console.log('p');
+    $('.bud__start-area-date, .bud__start-area-time, .bud__finish-area-date, .bud__finish-area-time').each(function(){
+      $(this).removeAttr('disabled');
+    });
+  }else if($(this).val() == 'start'){
+    console.log('s');
+    $('.bud__start-area-date, .bud__start-area-time').each(function(){
+      $(this).removeAttr('disabled');
+    });
+    $('.bud__finish-area-date, .bud__finish-area-time').each(function(){
+      $(this).attr('disabled', true);
+    });
+  }else if($(this).val() == 'finish'){
+    console.log('f');
+   $('.bud__start-area-date, .bud__start-area-time').each(function(){
+    $(this).attr('disabled', true);
+  });
+   $('.bud__finish-area-date, .bud__finish-area-time').each(function(){
+    $(this).removeAttr('disabled');
+  });
+ }
 });
